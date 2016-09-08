50407e405132a2e2f80adb3e96b3130ebfda3c0f
Amit Rai <amit.rai1095@gmail.com>
2016-09-08 14:13:36 +0530

    Changed Code and data persistency

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,35 +1,22 @@
-var electron = require('electron'), ipcRender = electron.ipcRenderer, ipcMainClient = electron.ipcMain
-var remote = electron.remote;
-var dialog = remote.require('electron').dialog;
-var fs = require('fs');
-var id3 = require('id3js');
-var musicmetadata = require('musicmetadata');
-var audioMetaData = require('audio-metadata');
-var min_button = document.getElementById('minimize');
-var max_button = document.getElementById('maximize');
-var close_button = document.getElementById('close');
-var file_option = document.getElementById('file-menu-option');
-var file_menu = document.getElementById('file-menu');
-var open_button = document.getElementById('open');
-var open_file_btn = document.getElementById('open-file-btn');
-var close_file_btn = document.getElementById('close-file-btn');
-var add_file_btn = document.getElementById('add-file-btn');
-var exit_button = document.getElementById('exit-btn');
-var play_button = document.getElementById('play-button');
-var seek = document.getElementById('seek');
-var albumCover = document.getElementById('album-banner');
-var songTitle = document.getElementById('song-title');
-var songAlbum = document.getElementById('song-album');
-var songArtist = document.getElementById('song-artist');
-var currentPlaylistSection = document.getElementById('current-playlist');
-var volumeSeek = document.getElementById('volume-seek')
-var file_options = document.getElementById('file-options');
-var remove_file_btn = document.getElementById('remove-file-btn');
-var repeat_btn = document.getElementById('repeat-btn')
+var electron = require('electron'), ipcRender = electron.ipcRenderer, ipcMainClient = electron.ipcMain,
+  remote = electron.remote, dialog = remote.require('electron').dialog, fs = require('fs'),
+  id3 = require('id3js'), musicmetadata = require('musicmetadata'), audioMetaData = require('audio-metadata')
+
+var min_button = document.getElementById('minimize'), max_button = document.getElementById('maximize'),
+  close_button = document.getElementById('close'), file_option = document.getElementById('file-menu-option'),
+  file_menu = document.getElementById('file-menu'), open_button = document.getElementById('open'),
+  open_file_btn = document.getElementById('open-file-btn'), close_file_btn = document.getElementById('close-file-btn'),
+  add_file_btn = document.getElementById('add-file-btn'), exit_button = document.getElementById('exit-btn'),
+  play_button = document.getElementById('play-button'), seek = document.getElementById('seek'),
+  albumCover = document.getElementById('album-banner'), songTitle = document.getElementById('song-title'),
+  songAlbum = document.getElementById('song-album'), songArtist = document.getElementById('song-artist'),
+  currentPlaylistSection = document.getElementById('current-playlist'), volumeSeek = document.getElementById('volume-seek'),
+  file_options = document.getElementById('file-options'), remove_file_btn = document.getElementById('remove-file-btn'),
+  repeat_btn = document.getElementById('repeat-btn')
+
 var isMaximized = false, menuActive = false, count = 0, path = "", songDuration, audio, timeLeft = 0,
-  timePlayed = 0, songIndex = 0, intervalTime = 1000, volume, seeking = false, rightClickTarget, subMenuActive = false, repeat = 0
-var songQueue = []
-var tableRow, rowElement, textElement
+  timePlayed = 0, songIndex = 0, intervalTime = 1000, volume, seeking = false, rightClickTarget, subMenuActive = false,
+  repeat = 0, songQueue = [], tableRow, rowElement, textElement, storage = window.localStorage
 
 
 // Event Listener to check if user clicks on a menu option when the menu is open
@@ -39,8 +26,8 @@ document.addEventListener('click', function(e){
       hideMenu()
     }
     else{
-      file_menu.style.display = "block";
-      menuActive = true;
+      file_menu.style.display = "block"
+      menuActive = true
     }
   }
   else if(e.target !=file_menu && menuActive && e.target != min_button && e.target != max_button && e.target != close_button){
@@ -48,8 +35,8 @@ document.addEventListener('click', function(e){
   }
 
   if(e.target != file_options && subMenuActive && e.target !=file_menu && menuActive && e.target != min_button && e.target != max_button && e.target != close_button){
-    file_options.style.display = "none";
-    subMenuActive = false;
+    file_options.style.display = "none"
+    subMenuActive = false
   }
 })
 // Event Listener to listen for minimize window activity
@@ -128,18 +115,25 @@ play_button.addEventListener('click', function(){
   if(audio){
     if(audio.paused){
       audio.play()
-      play_button.src = "../assets/images/controls/player/pause.png";
+      play_button.src = "../assets/images/controls/player/pause.png"
     }
     else{
       audio.pause()
-      play_button.src = "../assets/images/controls/player/play.png";
+      play_button.src = "../assets/images/controls/player/play.png"
     }
   }
 })
 $(document).ready(function(){
+  if(storage.getItem('volume')){
+    volume = storage.getItem('volume')
+    volumeSeek.value = volume
+  }
   $(volumeSeek).change(function(){
     volume = volumeSeek.value
-    audio.volume = volume
+    if(audio){
+      audio.volume = volume
+    }
+    storage.setItem('volume', volume)
   })
   $(seek).mousedown(function(){
     seeking = true
@@ -168,8 +162,8 @@ remove_file_btn.addEventListener('click', function(){
       playFile(songQueue[songIndex])
     }
     else{
-      audio.pause();
-      audio.currentTime = 0;
+      audio.pause()
+      audio.currentTime = 0
     }
   }
   $(file_options).css('display', 'none')
@@ -186,69 +180,70 @@ repeat_btn.addEventListener('click', function(){
       break;
     case 1:
       $(repeat_btn).css('color', 'rgba(255,255,255,1)')
-      break;
+      break
     case 2:
       $(repeat_btn).css('border', '1px solid rgba(255,255,255,1)')
-      break;
+      break
   }
 })
 
 
 // Function to check for the height of the window and recompute the height of the current playlist section
 function resizeContainer(){
-  var height = window.outerHeight;
-  height = height - 360;
-  currentPlaylistSection.style.height = height;
+  var height = window.outerHeight
+  height = height - 360
+  currentPlaylistSection.style.height = height
 }
 // Function to close the menu
 function hideMenu(){
-  file_menu.style.display = "none";
-  menuActive = false;
+  file_menu.style.display = "none"
+  menuActive = false
 }
 // Function to start playing the selected songs
 function playFile(path){
   if(audio){
-    audio.pause();
-    audio.currentTime = 0;
+    audio.pause()
+    audio.currentTime = 0
   }
-  audio = new Audio('file:///'+path);
+  audio = new Audio('file:///'+path)
 
   // Getting metadata for the audio file
   id3({ file: path.toString(), type: id3.OPEN_LOCAL }, function(err, tags) {
     if(tags){
-      songTitle.innerHTML = tags.title;
-      songAlbum.innerHTML = tags.album;
-      songArtist.innerHTML = tags.artist;
+      songTitle.innerHTML = tags.title
+      songAlbum.innerHTML = tags.album
+      songArtist.innerHTML = tags.artist
     }
-  });
+  })
 
   // Getting Album Cover of the audio file
   musicmetadata(fs.createReadStream(path.toString()), function (err, metadata) {
     if(!err && metadata.picture[0].data){
-      var base64Data = base64ArrayBuffer( metadata.picture[0].data );
-      albumCover.src = 'data:image/png;base64, ' + base64Data;
-      var img = "url('data:image/png;base64, "+base64Data + "')";
-      document.getElementById('player-background').style.backgroundImage = img;
+      var base64Data = base64ArrayBuffer( metadata.picture[0].data )
+      albumCover.src = 'data:image/png;base64, ' + base64Data
+      var img = "url('data:image/png;base64, "+base64Data + "')"
+      document.getElementById('player-background').style.backgroundImage = img
     }
-  });
-  songDuration = 0;
+  })
+  songDuration = 0
   audio.addEventListener('loadedmetadata', function() {
-      songDuration = audio.duration;
-      play_button.src = "../assets/images/controls/player/pause.png";
-      audio.play();
-      startSeek();
+      songDuration = audio.duration
+      play_button.src = "../assets/images/controls/player/pause.png"
+      audio.volume = volumeSeek.value
+      audio.play()
+      startSeek()
   });
   // audio.muted = true
 }
 // Function to start the seek
 function startSeek(){
-  seek.max = songDuration;
-  timeLeft = songDuration;
-  intervalTime = 1000;
+  seek.max = songDuration
+  timeLeft = songDuration
+  intervalTime = 1000
   setInterval(function(){
     if(!seeking){
       // Updating seek value
-      seek.value = audio.currentTime;
+      seek.value = audio.currentTime
     }
     // Playing next file if the current song has ended
     if(audio.ended && songIndex < songQueue.length){
@@ -286,7 +281,7 @@ function addToPlaylist(songsList){
         textElement = document.createTextNode(tags.artist)
         rowElement.appendChild(textElement)
         tableRow.appendChild(rowElement)
-        tableRow.setAttribute("id", i);
+        tableRow.setAttribute("id", i)
         document.getElementById('playlist-body').appendChild(tableRow)
       }
     });
